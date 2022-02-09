c232e93ea73b125cb5c7a93e7287bac2f5946d4d
Aravindan22 <aravindan7822@gmail.com>
2022-02-09 03:37:12 +0000

    Added MaterialUI

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
-
+import * as React from "react";
+import Header from './components/Header';
 function App() {
   return (
-    <div >
-      Calculator
+    <div>
+      <Header></Header>
     </div>
   );
 }
